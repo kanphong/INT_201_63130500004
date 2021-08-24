d54464e3335bb24d45cfc6bbb00e7ec0d55a7188
kanphong <kantaphong777@gmail.com>
2021-08-24 13:47:16 +0700

    practice in class week3

---
+++ 04_Objects/practice1.js
@@ -0,0 +1,29 @@
+let year = 1900;
+
+if(year % 12 == 0){
+    zodiac = "monkey";
+}else if(year % 12 == 1){
+    zodiac = "rooster";
+}else if(year % 12 == 2){
+    zodiac = "dog";
+}else if(year % 12 == 3){
+    zodiac = "pig";
+}else if(year % 12 == 4){
+    zodiac = "rat";
+}else if(year % 12 == 5){
+    zodiac = "ox";
+}else if(year % 12 == 6){
+    zodiac = "tiger";
+}else if(year % 12 == 7){
+    zodiac = "rabbit";
+}else if(year % 12 == 8){
+    zodiac = "dragon";
+}else if(year % 12 == 9){
+    zodiac = "snake";
+}else if(year % 12 == 10){
+    zodiac = "horse";
+}else if(year % 12 == 11){
+    zodiac = "sheep";
+}
+
+console.log(zodiac);
